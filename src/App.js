37008a9037a0519ac9d8da568920ab9c5546abc8
Karl Leonardo <karlleonardo27@gmail.com>
2023-04-10 03:07:40 +0800

    Fixed page routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import Header from './components/Header';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
+import ResultPage from './pages/ResultPage';
+
+import Header from './components/Header';
+import Loading from './components/Loading';
 
 function App() {
+  const { isLoading, error } = useAuth0();
+
+  if (error) {
+    return <div>Oops... {error.message}</div>;
+  }
+
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
-    <div className="App">
-      <Header />
-      {/* <LoginPage/> */}
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/result" element={<ResultPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
