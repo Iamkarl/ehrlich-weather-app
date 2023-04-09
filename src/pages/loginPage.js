061e1389b5ad92a32a535e8553fc7e954c13b32e
Karl Leonardo <karlleonardo27@gmail.com>
2023-04-10 03:10:32 +0800

    Added login and logout functionality

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,36 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import '../styles/loginPage.css'
 
 function LoginPage() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [viewForm, setViewForm] = useState(false);
 
-    const handleLogin = (event) => {
-        event.preventDefault();
-        // Perform login logic here
+    const { isAuthenticated, loginWithRedirect } = useAuth0();
+
+    if(isAuthenticated) {
+        window.location.href = 'http://localhost:3000/';
     }
-
-    const showLoginForm = () => {
-        setViewForm(true);
-    };
 
     return (
         <div className='container'>
             <div className='content'>
                 <p>Welcome to the weather forecast web application. Please login with your Github user to use the application and view the weather in your city.</p>
-                {!viewForm && <button className="btn" onClick={showLoginForm}>Login</button>}
-                {viewForm && <form onSubmit={handleLogin}>
-                    <label>
-                        Username:
-                        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-                    </label>
-                    <label>
-                        Password:
-                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                    </label>
-                    <button type="submit">Login</button>
-                </form>}
+                <button className="btn" onClick={loginWithRedirect}>Login</button>
             </div>
 
         </div>
